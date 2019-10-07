1a3b9c85fd8c90b6dd6a82ea864740a412380cc7
Yu SUGIMOTO <ysugimoto@idcf.jp>
2019-10-07 10:16:33 +0900

    Add input form.

--- webserver.go
+++ webserver.go
@@ -8,9 +8,14 @@ import (
 type Server struct {}
 
 func (Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	v := r.FormValue("input_value")
 	h := `
-<html><head><title>Hello</title></head><body>
-  Hello
+<html><head><title>HTMLのフォーム</title></head><body>
+  <form action="/" method="post">
+	<input type="text" name="input_value">
+	<input type="submit" name="送信"><br>
+	入力値：` + v + `
+  </form>
 </body></html>
 `
 	fmt.Fprint(w, h)
