4e8bed4bc88d3a7c3424111aeb6e08274bfc1fd0
Yu SUGIMOTO <ysugimoto@idcf.jp>
2019-10-07 14:11:08 +0900

    argument.

--- variadic.go
+++ variadic.go
@@ -2,16 +2,23 @@ package main
 
 import "fmt"
 
-func funcVariadic(n int, slice ...string) { //可変長引数
+func funcVariadic(n int, slice ...string) string { //可変長引数
 	fmt.Print(n, ": ")
 	for _, s := range slice {
 		fmt.Print(s, ", ")
 	}
 	fmt.Println()
+	return "true"
 }
 
 func main() {
 	slice := []string{"a", "b"}
 	funcVariadic(10, slice...) // スライサーというものを使う
-	funcVariadic(10, "a", "b")
+	fmt.Println(funcVariadic(10, "a", "b"))
+	fmt.Println(funcResult())
+}
+
+func funcResult() (result int) {
+	result = 100
+	return 20
 }
