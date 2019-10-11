fc2dc7de8f8a1860977b361a5de3b5514c9c30a7
Yu SUGIMOTO <ysugimoto@idcf.jp>
2019-10-11 14:01:01 +0900

    Add slice.

---
+++ slice.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []int {1,2,3}
+	slice2 := slice //参照渡し
+	slice[1] = 10 //参照渡しなのでslice2[1]も値が変わる
+
+	fmt.Println("slice: ", slice)
+	fmt.Println("slice2: ", slice2)
+}
+`
